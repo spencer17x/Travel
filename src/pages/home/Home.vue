c630d06c0fbf862ecbe5c9e2cabbb68a60833024
xuzepeng <1253478653@qq.com>
2018-07-29 00:12:33 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="home">
+		<div>hello world</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Home'
+	}
+</script>
+
+<style scoped>
+	.home {
+		font-size: 50px;
+	}
+</style>
